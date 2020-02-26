351db6fbce61224a0b7ad334a523c3f587ff4dcc
Jonatan Rodríguez Suárez <jonatanrs@mundiaudit.com>
2020-02-26 12:36:36 +0000

    Agregar archivos de proyecto.

---
+++ Pdf2Text.Support/PdfUtils.cs
@@ -0,0 +1,24 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using System;
+using System.Text;
+
+namespace Pdf2Text.Support
+{
+    public class PdfUtils
+    {
+        static public string Pdf2Text(string path)
+        {
+            using (var pdfReader = new PdfReader(path))
+            using (var pdfDocument = new PdfDocument(pdfReader))
+            {
+                var stringBuilder = new StringBuilder();
+
+                for (var i = 1; i<=pdfDocument.GetNumberOfPages(); i++)
+                    stringBuilder.Append(PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(i)));
+
+                return stringBuilder.ToString().Replace("\n", "\r\n");
+            }
+        }
+    }
+}
---
+++ Pdf2Text.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using Pdf2Text.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pdf2Text.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog() {
+                Filter = "PDF files (*.pdf)|*.pdf"
+            };
+
+            openFileDialog.FileOk += Dial_FileOk;
+
+            openFileDialog.ShowDialog();
+        }
+
+        private void Dial_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            textBox.Text = PdfUtils.Pdf2Text((sender as OpenFileDialog).FileName);
+        }
+    }
+}
